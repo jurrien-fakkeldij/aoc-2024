732bf8b5a2ee67553c1b5e3a43b7dcd17280f276
Jurrien Fakkeldij <jurrien.fakkeldij@gmail.com>
2024-12-01 14:38:14 +0100

    prepare day 2

--- src/solver.zig
+++ src/solver.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const Retriever = @import("./retriever.zig").Retriever;
 const Day1 = @import("./days/day1.zig").Day1;
+const Day2 = @import("./days/day2.zig").Day2;
 
 pub const Solver = struct {
     pub fn solve(allocator: std.mem.Allocator, day: u32) !void {
@@ -9,6 +10,11 @@ pub const Solver = struct {
                 const input = try Retriever.get(allocator, day);
                 defer allocator.free(input);
                 try Day1.solve(input);
+            },
+            2 => {
+                const input = try Retriever.get(allocator, day);
+                defer allocator.free(input);
+                try Day2.solve(input);
             },
             else => std.debug.print("Did not find the specified day to solve yet. Day {d} is not solvable yet, has this day already come out or did my creator (slacker) did not create the solution yet.\n", .{day}),
         }
