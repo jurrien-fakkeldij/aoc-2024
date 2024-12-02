c4d77cc4a9a8b863e4480522de324290ee905ec2
Jurrien Fakkeldij <jurrien.fakkeldij@gmail.com>
2024-12-02 16:32:27 +0100

    prepare day 3

---
+++ src/days/day3.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+pub const Day3 = struct {
+    const test_input: []const u8 =
+        \\3   4
+        \\4   3
+        \\2   5
+        \\1   3
+        \\3   9
+        \\3   3
+    ;
+
+    pub fn solve(input: []const u8) !void {
+        std.debug.print("Solving Day 1\n", .{});
+        try solvePart1(input);
+        try solvePart2(input);
+    }
+
+    fn solvePart1(_: []const u8) !void {
+        std.debug.print("Part 1\n", .{});
+
+        const total_test: u32 = 0;
+        std.debug.print("test: {d} \n", .{total_test});
+
+        const total: u32 = 0;
+        std.debug.print("answer: {d} \n", .{total});
+    }
+
+    fn solvePart2(_: []const u8) !void {
+        std.debug.print("Part 2\n", .{});
+        const total_test: i32 = 0;
+        std.debug.print("test: {d} \n", .{total_test});
+
+        const total: i32 = 0;
+        std.debug.print("answer: {d} \n", .{total});
+    }
+};
--- src/solver.zig
+++ src/solver.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const Retriever = @import("./retriever.zig").Retriever;
 const Day1 = @import("./days/day1.zig").Day1;
 const Day2 = @import("./days/day2.zig").Day2;
+const Day3 = @import("./days/day3.zig").Day3;
 
 pub const Solver = struct {
     pub fn solve(allocator: std.mem.Allocator, day: u32) !void {
@@ -15,6 +16,11 @@ pub const Solver = struct {
                 const input = try Retriever.get(allocator, day);
                 defer allocator.free(input);
                 try Day2.solve(input);
+            },
+            3 => {
+                const input = try Retriever.get(allocator, day);
+                defer allocator.free(input);
+                try Day3.solve(input);
             },
             else => std.debug.print("Did not find the specified day to solve yet. Day {d} is not solvable yet, has this day already come out or did my creator (slacker) did not create the solution yet.\n", .{day}),
         }
